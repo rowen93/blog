ed80d7aa5cc1daa07e17ac3854e1aec3bfe99f99
Rebecca Owen <rebeccaowen93@hotmail.co.uk>
2020-12-14 20:15:07 +0000

    Pagination

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 def home(request):
     post_list = Post.objects.all().order_by("-date_posted")
@@ -23,10 +23,21 @@ class PostListView(ListView):
     template_name = "blog/home.html" #looks for the view in this order <app>/<model>_<viewtype>.html
     context_object_name = "posts"
     ordering = ["-date_posted"]
+    paginate_by = 3
+
 
 class PostDetailView(DetailView):
     model = Post
+
     
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
 
 def about(request):
     return render(request, "blog/about.html", {"title": "about"})
